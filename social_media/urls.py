e2a5e0c04a27aade1c119f95cb8b1febf8f666f3
Vladyslav Demydenko <bublikyo3@gmail.com>
2023-04-23 21:11:22 +0200

    Prevent users from changing or deleting a non-own post, add Comment create view and url

--- social_media/urls.py
+++ social_media/urls.py
@@ -1,19 +1,16 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from social_media.views import PostViewSet, CommentCreateView, HashtagViewSet
+from social_media.views import PostViewSet, CommentSetView, HashtagViewSet, CommentCreateView
 
 router = routers.DefaultRouter()
 router.register("posts", PostViewSet)
 router.register("hashtags", HashtagViewSet)
+router.register("comments", CommentSetView)
 
 urlpatterns = [
     path("", include(router.urls)),
-    path(
-        "posts/<int:pk>/comment/create/",
-        CommentCreateView.as_view(),
-        name="comment-create",
-    ),
+    path("posts/<int:pk>/comment/create/", CommentCreateView.as_view(), name="comment_create"),
 ]
 
 app_name = "social_media"
--- social_media/views.py
+++ social_media/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, generics, status, mixins
 from rest_framework.decorators import action
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from social_media.models import Post, Comment, Hashtag
 from social_media.serializers import (
-    PostCreateSerializer,
     PostListSerializer,
     CommentSerializer,
     PostDetailSerializer,
     CommentListSerializer,
     HashtagSerializer,
+    PostSerializer,
 )
+from user.permissions import IsStaffOrReadOnly
 
 
 def ids_from_string(string_ids: str) -> list:
@@ -20,8 +22,9 @@ def ids_from_string(string_ids: str) -> list:
 
 
 class PostViewSet(viewsets.ModelViewSet):
-    serializer_class = PostCreateSerializer
+    serializer_class = PostSerializer
     queryset = Post.objects.all()
+    permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         hashtags = self.request.query_params.get("hashtags")
@@ -63,6 +66,53 @@ class PostViewSet(viewsets.ModelViewSet):
         post.liked_by.remove(request.user)
         return Response({"detail": "Post unliked successfully."})
 
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.user != self.request.user:
+            raise PermissionDenied(
+                "You can only delete posts that you have created."
+            )
+
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.user != self.request.user:
+            raise PermissionDenied(
+                "You can only update posts that you have created."
+            )
+
+        return super().update(request, *args, **kwargs)
+
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.user != self.request.user:
+            raise PermissionDenied(
+                "You can only partially update posts that you have created."
+            )
+
+        return super().partial_update(request, *args, **kwargs)
+
+
+class CommentSetView(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet
+):
+    queryset = Comment.objects.all()
+    serializer_class = CommentListSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def perform_create(self, serializer):
+        post_id = self.kwargs.get("pk")
+        serializer.save(user=self.request.user, post_id=post_id)
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
 
 class CommentCreateView(generics.CreateAPIView):
     queryset = Comment.objects.all()
@@ -77,3 +127,4 @@ class CommentCreateView(generics.CreateAPIView):
 class HashtagViewSet(viewsets.ModelViewSet):
     queryset = Hashtag.objects.all()
     serializer_class = HashtagSerializer
+    permission_classes = [IsStaffOrReadOnly]
