73f688d8256856a500d8b6d1c223ade603cb1a36
Vladyslav Demydenko <bublikyo3@gmail.com>
2023-04-23 16:47:23 +0200

    Add filtering posts by hashtags ids

--- social_media/views.py
+++ social_media/views.py
@@ -5,9 +5,25 @@ from social_media.models import Post
 from social_media.serializers import PostCreateSerializer, PostListSerializer
 
 
+def ids_from_string(string_ids: str) -> list:
+    return [int(str_id) for str_id in string_ids.split(",")]
+
+
 class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostCreateSerializer
     queryset = Post.objects.all()
+
+    def get_queryset(self):
+        hashtags = self.request.query_params.get("hashtags")
+        queryset = self.queryset
+
+        if hashtags:
+            hashtags_ids = ids_from_string(hashtags)
+            queryset = queryset.filter(
+                hashtags__id__in=hashtags_ids
+            )
+
+        return queryset.distinct()
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
