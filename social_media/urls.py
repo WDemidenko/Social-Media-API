5f229ec5e80e7d18604284c04c1dd59cc40cdd89
Vladyslav Demydenko <bublikyo3@gmail.com>
2023-04-23 18:11:24 +0200

    Add view and url for creating comment

--- social_media/urls.py
+++ social_media/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from social_media.views import PostViewSet
+from social_media.views import PostViewSet, CommentCreateView
 
 router = routers.DefaultRouter()
 router.register("posts", PostViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path(
+        "posts/<int:pk>/comment/create/",
+        CommentCreateView.as_view(),
+        name="comment-create",
+    ),
 ]
 
 app_name = "social_media"
--- social_media/views.py
+++ social_media/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from rest_framework import viewsets, generics
 from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from social_media.models import Post
-from social_media.serializers import PostCreateSerializer, PostListSerializer
+from social_media.models import Post, Comment
+from social_media.serializers import (
+    PostCreateSerializer,
+    PostListSerializer,
+    CommentSerializer,
+    PostDetailSerializer,
+    CommentListSerializer,
+)
 
 
 def ids_from_string(string_ids: str) -> list:
@@ -21,9 +28,7 @@ class PostViewSet(viewsets.ModelViewSet):
 
         if hashtags:
             hashtags_ids = ids_from_string(hashtags)
-            queryset = queryset.filter(
-                hashtags__id__in=hashtags_ids
-            )
+            queryset = queryset.filter(hashtags__id__in=hashtags_ids)
 
         return queryset.distinct()
 
@@ -34,6 +39,9 @@ class PostViewSet(viewsets.ModelViewSet):
         if self.action == "list":
             return PostListSerializer
 
+        if self.action == "retrieve":
+            return PostDetailSerializer
+
         return self.serializer_class
 
     @action(detail=True, methods=["get"])
@@ -43,7 +51,7 @@ class PostViewSet(viewsets.ModelViewSet):
         """
         post = self.get_object()
         post.liked_by.add(request.user)
-        return Response({'detail': "Post liked successfully."})
+        return Response({"detail": "Post liked successfully."})
 
     @action(detail=True, methods=["get"])
     def unlike(self, request, pk=None):
@@ -53,3 +61,13 @@ class PostViewSet(viewsets.ModelViewSet):
         post = self.get_object()
         post.liked_by.remove(request.user)
         return Response({"detail": "Post unliked successfully."})
+
+
+class CommentCreateView(generics.CreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentListSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def perform_create(self, serializer):
+        post_id = self.kwargs.get("pk")
+        serializer.save(user=self.request.user, post_id=post_id)
